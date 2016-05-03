3de2df39efa74448f2cfb4dd706278743608af26
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-05-02 23:26:39 -0300

    Project

---
+++ app/src/main/java/com/sibela/examples/intentfilter/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.sibela.examples.intentfilter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Bind(R.id.text)
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
+
+        text.setText(getIntent().getStringExtra("message"));
+    }
+}
